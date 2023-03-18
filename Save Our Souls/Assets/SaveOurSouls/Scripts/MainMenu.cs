616b713b919f2b34a53811b68a2da7464bd3a1de
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-18 13:27:44 +0530

    Button fcuntionality - MainMenu.unity

---
+++ Save Our Souls/Assets/SaveOurSouls/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void RefugeeMap()
+    {
+        SceneManager.LoadScene("Refugee Map");
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene("About Menu");
+    }
+
+    
+    public void Exit()
+    {
+     // save any game data here
+     #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+     
+     }
+  
+}
